a3a3c8ef0ba6a8b0e8fe252ff4030f81009827bd
Riccardo Biscaro <Zendrow@users.noreply.github.com>
2018-04-27 23:30:02 +0200

    97 - pwa

---
+++ scripts/serviceWorker.js
@@ -0,0 +1,29 @@
+function registerServiceWorker() {
+    // register sw script in supporting browsers
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('serviceWorker.js', { scope: '/' }).then(() => {
+        console.log('Service Worker registered successfully.');
+      }).catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+    }
+  }
+  
+
+  self.addEventListener('install', e => {
+    e.waitUntil(
+      // after the service worker is installed,
+      // open a new cache
+      caches.open('my-pwa-cache').then(cache => {
+        // add all URLs of resources we want to cache
+        return cache.addAll([
+          '/',
+          '/index.html',
+         // '/about.html',
+         // '/images/doggo.jpg',
+          '/styles/main.css',
+          '/scripts/main.js',
+        ]);
+      })
+    );
+   });
